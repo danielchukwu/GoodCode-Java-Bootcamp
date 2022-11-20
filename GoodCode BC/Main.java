a330a0a7d231634bfd0300f979c0d572459f7054
dc <00danzy@gmail.com>
2022-11-20 08:21:26 +0100

    update

--- GoodCode BC/Main.java
+++ GoodCode BC/Main.java
@@ -1,38 +1,9 @@
-import java.util.Scanner;
-
-
 public class Main {
+   // c.
    public static void main(String[] args) {
-   //    Scanner sc = new Scanner(System.in);
-
-   //    System.out.println("Welcome to NGK");
-   //    int bill = 0;
-
-   //    System.out.println("What would you like to order: ");
-   //    String food = sc.next();
-
-   //    if (food.equals("rice")){
-   //       System.out.println("You ordered: " + food);
-   //       bill += 450;
-   //    } else if (food.equals("beans")){
-   //       System.out.println("You ordered: " + food);
-   //       bill += 400;
-   //    } else if (food.equals("yam")){
-   //       System.out.println("You ordered: " + food);
-   //       bill += 350;
-   //    } else {
-   //       System.out.println("You Ordered: " + food);
-   //       bill += 600;
-   //    }
+      // CODING CHALLENGE 12
       
-   //    System.out.println("Total Bill: " + bill);
-
-      double up = 8;
-      double down = 15;
-      double result = (up / 15) * 100;
-
-      System.out.println(result);
-
    }
 }
 
+// c. Difficulty: Hard
