5588ae23e271a7be9659f6dd0deec24c6c61e16e
dc <00danzy@gmail.com>
2022-11-23 10:50:03 +0100

    update

--- GoodCode BC/CC/CC12.java
+++ GoodCode BC/CC/CC12.java
@@ -1,82 +1,94 @@
 package CC;
 
-import java.util.Scanner;
+public class CC11 {
 
-public class CC12 {
    // a.
-   private static int getRandomNumber(int num) {
-      double randomNumber = Math.random() * num;
-      return (int) randomNumber;    // (int) converts randomNumber from (double) to (int)
+   public static int getNumber(){
+      return 5;
+   }
+   public static String getText(){
+      return "Hello World.";
+   }
+   public static boolean getBoolean(){
+      return true;
+   }
+   
+   // b.
+   // Add two numbers function
+   public static double addTwoNumbers(double a, double b) {
+      double solution = a + b;
+      return solution;
+   }
+   // Subtract two numbers function
+   public static double subTwoNumbers(double a, double b) {
+      double solution = a - b;
+      return solution;
    }
 
-   // b. 
-   // Log function
-   private static double findLog(double value) {
-      return Math.log(value);
-   }
-   // Sine function
-   private static double findSine(double value) {
-      return Math.sin(value);
-   }
-   // Square root function
-   private static double findSquareRoot(double value) {
-      return Math.sqrt(value);
-   }   
-
-   // c. 
-   // Get random pick for cpu. Either rock, paper or scissors
-   public static String getRandomPick() {
-      int cpu_guess = (int) Math.random() * 3;   // (int) says convert Math.random() * 3 return value in (double) to (int)
-      switch(cpu_guess) {
-         case (0): return "rock";
-         case (1): return "paper";
-         case (2): return "scissors";
-         default: return "";
-      }
-   }
-
-   // Print the Winner
-   public static void printWinner(String userPick, String cpuPick) {
-      System.out.println("User Picked: " + userPick + "\nCpu Picked: " + cpuPick);
-      if (userPick.equals(cpuPick)){
-         System.out.println("It's a draw.");
-      } else if (userPick.equals("rock") && cpuPick.equals("scissors")){
-         System.out.println("Rock smashes scissors. You WIN.");
-      } else if (userPick.equals("paper") && cpuPick.equals("rock")){
-         System.out.println("Paper covers rock. You WIN.");
-      } else if (userPick.equals("scissors") && cpuPick.equals("paper")){
-         System.out.println("Scissors cuts paper into pieces. You WIN.");
-      } else {
-         System.out.println("Wrong pick. You LOOSE.");
+   // c.
+   /**
+    * Function name: fahrenheitCelsiusConverter
+    *
+    * @param value           (double)
+    * @param conversionType  (double)
+    * @return                (double)
+    *
+    * Inside the Function:
+    * 1. Converts fahrenheit to Celsius when the second argument equals 'f'
+    * 2. Converts Celsius to fahrenheit when the second argument equals 'c'
+    */
+   public static double fahrenheitCelsiusConverter(double value, char conversionType) {
+      switch (conversionType){
+         case ('f'):
+            // (32°F − 32) × 5/9 = 0°C
+            double fahrenheitToCelsius = (value - 32) * 5/9;
+            return fahrenheitToCelsius;
+         case ('c'):
+            // (0°C × 9/5) + 32 = 32°F
+            double celsiusToFahrenheit = (value * 9/5) + 32;
+            return celsiusToFahrenheit;
+         default:
+            return 0.0;
       }
    }
 
    public static void main(String[] args) {
       // CODING CHALLENGE 12
-      // a. Difficulty: Easy
-      Scanner scan = new Scanner(System.in);
-      System.out.print("\nEnter any number: ");
-      int numberInput = scan.nextInt();
 
-      int randomNumResult = getRandomNumber(numberInput);
-      System.out.println("Random Number Between 0 and " + numberInput + " is = " + randomNumResult);
-      scan.close();
+      // a. Difficulty: Easy
+      int num = getNumber();
+      String text = getText();
+      boolean bool = getBoolean();
+      System.out.println("Num: " + num + "\nText: " + text + "\nBool: " + bool);
 
       // b. Difficulty: Medium
-      double r1 = findLog(50);
-      double r2 = findSine(90);
-      double r3 = findSquareRoot(25);
-      System.out.println("log50 = " + r1 + "\nsin(90) = " + r2 + "\n√25 = " + r3);
+      double addNum = addTwoNumbers(10, 14.5);
+      double subNum = subTwoNumbers(4, -5.5);
+      System.out.println("addNum Result = " + addNum + "\nsubNum Result = " + subNum);
 
       // c. Difficulty: Hard (optional)
-      System.out.println("\nWelcome to a game of Rock Paper Scissors.");
-      Scanner sc = new Scanner(System.in);
-      System.out.print("\nEnter rock, paper or scissors: ");
-      String userPick = sc.nextLine();
-      String cpuPick = getRandomPick();       // function call
 
-      printWinner(userPick, cpuPick);         // function call
-      System.out.println("\nWritten By Daniel.\n");
-      sc.close();
+      double result1 = fahrenheitCelsiusConverter(32, 'f');
+      double result2 = fahrenheitCelsiusConverter(0, 'c');
+      System.out.println(32 + "°F To Celsius = " + result1 + "°C");
+      System.out.println(0  + "°C To Fahrenheit = " + result2 + "°F");      
+      
+      System.out.println("\nWritten by Daniel");
    }
 }
+
+// 12. CODING CHALLENGE (Module 8: Tutorial 6 - 10)
+
+// a. Difficulty: Easy
+// Create a function that returns the an integer value
+// Create a function that returns a string value
+// Create a function that returns a boolean value
+
+// b. Difficulty: Medium
+// Create a function that 
+// - adds two numbers and returns the result
+// - subtracts two numbers and returns the result
+
+// c. Difficulty: Hard (optional)
+// Create a function that converts fahrenheit to Celsius or Celsius to fahrenheit and then returns the result
+// add a docString to these function (Attempt only if you watched yesterdays tutorial and you fully understand docStrings/docComments)
