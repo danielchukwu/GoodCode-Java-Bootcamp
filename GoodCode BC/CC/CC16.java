e9fd8e4b25561e5b2d47b0b198bb841bb9f80d9e
dc <00danzy@gmail.com>
2022-11-24 08:36:02 +0100

    update

--- GoodCode BC/CC/CC16.java
+++ GoodCode BC/CC/CC16.java
@@ -68,3 +68,54 @@ public class CC16 {
       }
    }
 }
+
+
+// 16. CODING CHALLENGE (Module 10: Tutorial 20 - 23 && Module 11: Tutorial 1 - 4)
+
+// a. Difficulty: Easy 🍔
+// create a function that prints from 99 down to 00 as shown below
+// >> 9 9
+// >> 9 8
+// >> ...
+// >> 1 2
+// >> 1 1
+// >> ...
+// >> 0 1
+// >> 0 0
+
+// b. Difficulty: Medium ⚒️ / Hard 🪨
+// Create a function that finds. Ask a user what they would like to calculate (e.g sine, log, square root) and then take an
+// input and calculate it, Ask if they would like to continue calculation
+// - the log of an input
+// - the sine of an input
+// - the square root of an input    (Tip: use the Math.<something>)
+
+// >> Calculator
+
+// >> What would you like to calculate? (sine, log square root)
+// >> sine
+
+// >> Which Number?
+// >> 40
+
+// >> sin(40) = 0.7451131604793488
+
+// >> Would you like to use this calculator again? (yes or no)
+// >> daef
+// >> invalid input!
+
+// >> Would you like to use this calculator again? (yes or no)
+// >> yes
+
+// >> What would you like to calculate? (sine, log square root)
+// >> log
+
+// >> Which Number?
+// >> 40
+
+// >> log(40) = 3.6888794541139363 
+
+// >> Would you like to use this calculator again? (yes or no)
+// >> no
+
+// >> Turned Off Casio Calculator
