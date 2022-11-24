cb35233610454eca83bec68b1e70b600be1c9383
dc <00danzy@gmail.com>
2022-11-24 08:35:53 +0100

    update

--- GoodCode BC/Main.java
+++ GoodCode BC/Main.java
@@ -1,44 +1,9 @@
 import java.util.Scanner;
+
 public class Main {
-   // b
-   
-   
    public static void main(String[] args) {
-      // 16. CODING CHALLENGE (Module 10: Tutorial 20 - 23)
+      // 17. CODING CHALLENGE (Module 12: Tutorial 1 - 7)
       
    }
 }
-// b. Difficulty: Medium / Hard
-// Create a function that finds. Ask a user what they would like to calculate (e.g sine, log, square root) and then take an
-// input and calculate it, Ask if they would like to continue calculation
-// - the log of an input
-// - the sine of an input
-// - the square root of an input    (Tip: use the Math.<something>)
-
-// >> Calculator
-// >> What would you like to calculate? (sine, log square root)
-// >> sine
-
-// >> Which Number?
-// >> 40
-
-// >> sin(40) = 0.7451131604793488
-// >> Would you like to use this calculator again? (yes or no)
-// >> daef
-// >> invalid input!
-
-// >> Would you like to use this calculator again? (yes or no)
-// >> yes
-
-// >> What would you like to calculate? (sine, log square root)
-// >> log
-
-// >> Which Number?
-// >> 40
-
-// >> log(40) = 3.6888794541139363
-
-// >> Would you like to use this calculator again? (yes or no)
-// >> no
-
-// >> Turned Off Casio Calculator
+// c. Difficulty: Hard
