55cab83420b464f29ceacea05000ae1594c95b77
dc <00danzy@gmail.com>
2022-11-25 08:36:54 +0100

    update

--- GoodCode BC/Main.java
+++ GoodCode BC/Main.java
@@ -1,9 +1,15 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
+   
    public static void main(String[] args) {
-      // 17. CODING CHALLENGE (Module 12: Tutorial 1 - 7)
+      // 18. CODING CHALLENGE (Module 12: Tutorial 8 - 15)
       
    }
 }
-// c. Difficulty: Hard
+// c. Difficulty: Hard (Extending Array) (optional)
+// Create a function that copies all the items from array1 and array2 to array3 and returns array3. (Tip: for loop required)
+// >> array1 = ["apple", "banana", "orange"]
+// >> array2 = ["guava", "paw paw", "pears", "water melon"]
+
+// >> array3 = ["apple", "banana", "orange", "guava", "paw paw", "pears", "water melon"]
