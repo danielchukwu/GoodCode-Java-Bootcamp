6c68fc6a20fc209410ab95ac9d59a5f97a3bdff2
dc <00danzy@gmail.com>
2022-11-26 23:22:32 +0100

    update

--- GoodCode BC/Main.java
+++ GoodCode BC/Main.java
@@ -1,15 +1,24 @@
-import java.util.Arrays;
-
 public class Main {
    
    public static void main(String[] args) {
-      // 18. CODING CHALLENGE (Module 12: Tutorial 8 - 15)
+      // 19. CODING CHALLENGE (Module 12: Tutorial 16 - 20)
       
    }
 }
-// c. Difficulty: Hard (Extending Array) (optional)
-// Create a function that copies all the items from array1 and array2 to array3 and returns array3. (Tip: for loop required)
-// >> array1 = ["apple", "banana", "orange"]
-// >> array2 = ["guava", "paw paw", "pears", "water melon"]
+// 19. CODING CHALLENGE (Module 12: Tutorial 16 - 20)
+// c. Difficulty: Hard   
+// Create a 2D array containing the grades of 3 students that are offering 4 courses
 
-// >> array3 = ["apple", "banana", "orange", "guava", "paw paw", "pears", "water melon"]
+// courses   1   2   3   4
+// sam      40  64  33  68
+// miguel   91  78  75  82
+// johnny   28  27  32  59
+
+// i. Using 3 print statements, print all the items on the diagonal
+// >> 40
+// >>   78
+// >>     32
+// ii. Using nested loops loop through the above 2d array and add 5 to each of the scores
+// >> 45  69  38  73
+// >> 96  83  80  87
+// >> 33  32  42  64
