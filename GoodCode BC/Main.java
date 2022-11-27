bf8f74d70f89694c7b5109677bff9ac7a9872dc4
dc <00danzy@gmail.com>
2022-11-27 23:00:12 +0100

    udpate

--- GoodCode BC/Main.java
+++ GoodCode BC/Main.java
@@ -1,24 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
    
    public static void main(String[] args) {
-      // 19. CODING CHALLENGE (Module 12: Tutorial 16 - 20)
+      // 20. CODING CHALLENGE (Module 12: Tutorial 21 - 26)
       
    }
 }
-// 19. CODING CHALLENGE (Module 12: Tutorial 16 - 20)
-// c. Difficulty: Hard   
-// Create a 2D array containing the grades of 3 students that are offering 4 courses
+// c. Difficulty: Hard
+// Write a program that uses two-dimensional arrays (3 rows & 4 columns) to store user account informations as shown below.
 
-// courses   1   2   3   4
-// sam      40  64  33  68
-// miguel   91  78  75  82
-// johnny   28  27  32  59
+// >> user1: [null, null, null, null]
+// >> user2: [null, null, null, null]
+// >> user3: [null, null, null, null]
 
-// i. Using 3 print statements, print all the items on the diagonal
-// >> 40
-// >>   78
-// >>     32
-// ii. Using nested loops loop through the above 2d array and add 5 to each of the scores
-// >> 45  69  38  73
-// >> 96  83  80  87
-// >> 33  32  42  64
+// // Repeat the below 3 times and populate our above two-dimensional array 
+
+// >> Enter username: daniel
+// >> Enter password: password12345
+// >> Enter email: daniel@gmail.com
+// >> Enter bio: it's all good
+
+// >> Final Result:
+// >>        username |  password   |     email       |    bio      | 
+// >> user1: [daniel,  password12345, daniel@gmail.com, it's all good]
+// >> user2: [albert,  pass123wordGo, albert@gmail.com, life is easy ohh]
+// >> user3: [imoney,  seeMoneyhere_, imoney@gmail.com, I want to feel like i cashed out]
