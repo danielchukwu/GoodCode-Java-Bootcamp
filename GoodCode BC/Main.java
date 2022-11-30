763434b79c99117b6ef26e65a7b495fa22f5f78c
dc <00danzy@gmail.com>
2022-11-30 08:26:11 +0100

    update

--- GoodCode BC/Main.java
+++ GoodCode BC/Main.java
@@ -1,28 +1,4 @@
-import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
    
-   public static void main(String[] args) {
-      // 20. CODING CHALLENGE (Module 12: Tutorial 21 - 26)
-      
-   }
 }
-// c. Difficulty: Hard
-// Write a program that uses two-dimensional arrays (3 rows & 4 columns) to store user account informations as shown below.
-
-// >> user1: [null, null, null, null]
-// >> user2: [null, null, null, null]
-// >> user3: [null, null, null, null]
-
-// // Repeat the below 3 times and populate our above two-dimensional array 
-
-// >> Enter username: daniel
-// >> Enter password: password12345
-// >> Enter email: daniel@gmail.com
-// >> Enter bio: it's all good
-
-// >> Final Result:
-// >>        username |  password   |     email       |    bio      | 
-// >> user1: [daniel,  password12345, daniel@gmail.com, it's all good]
-// >> user2: [albert,  pass123wordGo, albert@gmail.com, life is easy ohh]
-// >> user3: [imoney,  seeMoneyhere_, imoney@gmail.com, I want to feel like i cashed out]
