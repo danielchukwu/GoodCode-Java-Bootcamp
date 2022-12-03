603b1f13b755cdd9d9258f332e7523d877c580cb
dc <00danzy@gmail.com>
2022-12-03 16:29:55 +0100

    first commit

---
+++ GoodCode BC/CC/CC23Human.java
@@ -0,0 +1,21 @@
+package CC;
+
+public class CC23Human {
+   // a. Difficulty: Easy
+   String gender;
+   String ethnicity;
+   String name;
+   int dayOfBirth;
+   double height;
+   boolean isChristian;
+}
+// a. Difficulty: Easy 🍔
+// Create a Human Class with at least 4 variables and then create at least 2 objects from the class. (Get Creative)
+// Then make sure to print the below
+
+// >> Hello there, My name is <name>
+// >> Ethnicity: <ethnicity>
+// >> Gender: <gender>
+// >> Day Of Birth: <day>
+// >> Height: <height>cm
+// >> Christian: <true/false>
