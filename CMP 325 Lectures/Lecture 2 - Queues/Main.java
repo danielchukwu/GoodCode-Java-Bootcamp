dad9bc5097b90d711750d87002154e0d04e91af6
dc <00danzy@gmail.com>
2022-12-09 08:30:50 +0100

    first commit

---
+++ CMP 325 Lectures/Lecture 2 - Queues/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+   public static void main(String[] args) {
+      // Queue
+      Queue queue = new Queue();
+      queue.enQueue(5);
+      queue.enQueue(1);
+      queue.enQueue(9);
+      queue.enQueue(3);
+      queue.enQueue(7);
+      queue.enQueue(2);
+      queue.enQueue(6);
+      queue.enQueue(8);
+
+      queue.deQueue();
+      queue.deQueue();
+      queue.deQueue();
+
+      queue.displayQueue();
+   }
+}
+   
