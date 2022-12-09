ce21f77442d8996551a0fff86e94d8340ccb5601
dc <00danzy@gmail.com>
2022-12-09 08:31:14 +0100

    first commit

---
+++ CMP 325 Lectures/Lecture 2 - Stacks/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+   public static void main(String[] args) {
+      // Stack
+      Stack stack = new Stack(5);
+      stack.push(5);
+      stack.push(3);
+      stack.push(8);
+      stack.push(4);
+      stack.push(3);
+
+      stack.pop();
+      stack.pop();
+   }
+}
