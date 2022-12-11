5e271e067e86b6cc95b076d7a66f5a2067059815
dc <00danzy@gmail.com>
2022-12-11 07:18:03 +0100

    first commit

---
+++ GoodCode BC/CC/CC26Main.java
@@ -0,0 +1,32 @@
+package CC;
+
+public class CC26Main {
+   public static void main(String[] args) {
+      // A list of Car objects
+      CC26Car[] cars = {
+         new CC26Car("Mercedes", 15000, 2016, "black", new String[] {"tires", "stirring wheels"}),
+         new CC26Car("Bmw", 18000, 2015, "white", new String[] {"jack", "stirring wheels"}),
+         new CC26Car("Toyota", 11000, 2018, "red", new String[] {"tires"})
+      };
+
+      // Dealership object created
+      CC26Dealership dealership = new CC26Dealership();
+
+      // Add the list of cars object into our dealership object
+      for (int i = 0; i < cars.length; i++) {
+         dealership.setCars(cars[i], i);
+      }
+      
+      // print out the 1st and 3rd car in our dealership
+      System.out.println(dealership.getCar(0));;
+      System.out.println(dealership.getCar(2));;
+   }
+}
+
+// 26. CODING CHALLENGE (Module 16: Tutorial 31 - 40)
+// a.
+// Create a car class with the following attributes (Make, Price, Year, Color, Parts (an Array of car parts))
+// b.
+// Add a constructor, a copy constructor, setters and getters, a toString method
+// c. 
+// Create a dealership class that manages the Car class you created above, add setters and getters to the dealership class
