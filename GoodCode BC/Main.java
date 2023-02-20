47befbc1af809d4d87caefed014da8542ea55ee2
dc <00danzy@gmail.com>
2023-02-19 21:32:45 -0300

    update

--- GoodCode BC/Main.java
+++ GoodCode BC/Main.java
@@ -1,36 +1,33 @@
-import java.util.Arrays;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
-      
+      System.out.println("Hello World!");
+
+      try {
+         // loadData("Greetings.txt");
+         loadURL("https://www.google.com/images");
+      } catch (MalformedURLException e) {
+         System.out.println(e.getMessage());
+      }
+   }
+
+   public static void loadData(String name) throws FileNotFoundException {
+      FileInputStream fs = new FileInputStream(name);
+      Scanner scan = new Scanner(fs);
+      while(scan.hasNextLine()){
+         System.out.println(scan.nextLine());
+      }
+      scan.close();
+   }
+   
+   public static void loadURL(String link) throws MalformedURLException {
+      URL url = new URL(link);
+      System.out.println(url.getProtocol());
+      System.out.println(url.getHost());
+      System.out.println(url.getPath());
    }
 }
-
-// 31. CODING CHALLENGE (Module 18: Tutorial 21 - 30)
-// c. Difficulty: Hard
-// The below code is supposed to print 0 across the diagonal
-// [_, _, _]        [O, _, X]
-// [_, _, _]  --->  [_, O, _]
-// [_, _, _]        [X, _, 0]
-
-// But instead it prints
-// The below code is supposed to print 0 across the diagonal
-// [_, _, _]        [X, _, O]
-// [_, _, _]  --->  [_, X, _]
-// [_, _, _]        [O, _, X]
-
-// public static void main(String[] args) {
-//    char[][] array = new char[][] {
-//       {'_', '_', '_'},
-//       {'_', '_', '_'},
-//       {'_', '_', '_'},
-//    };
-
-//    for (int i=0; i < array.length; i++){
-//       array[i][2-i] = 'O';
-//       array[2-i][2-i] = 'X';
-//    }
-
-//    printArray(array);
-//    System.out.println("O WINS!");
-// }
